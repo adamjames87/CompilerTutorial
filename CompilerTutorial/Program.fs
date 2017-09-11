a4f48faabdcde498a29edb134889e80565c015aa
AdamJ <adamjames87@gmail.com>
2017-09-11 16:28:21 +0100

    Added lexer code

--- CompilerTutorial/Program.fs
+++ CompilerTutorial/Program.fs
@@ -1,7 +1,8 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
-
+open lexer
 open Microsoft.FSharp.Collections
+open Microsoft.FSharp.Text.Lexing
 
 type id = string
 type binop = Plus | Minus | Times | Div
@@ -64,14 +65,37 @@ let interp stm =
 
 
 let testStm1 = PrintStm [ NumExp(4);NumExp(5) ]
-let testStm2 = CompoundStm(AssignStm("a", NumExp(11)), PrintStm [ IdExp("a") ])
+let testStm2 = CompoundStm(AssignStm("a", NumExp(11)), CompoundStm(AssignStm("b", NumExp(12)), PrintStm [ OpExp(IdExp("a"), Plus, IdExp("b")) ]))
 
 let test1 = interp testStm1 
 let test2 = interp testStm2
 
+
+
+// Exercise 1.1
+//
+type key = Str
+
 printfn "A Test"
+
+let rec toTokenList tokenList tokenbuf = 
+    let next = lexer.tokenize tokenbuf
+    match next with
+    | Eof -> tokenList
+    | _ as t -> 
+        let newlist = t :: tokenList
+        toTokenList newlist tokenbuf
+
+let toTokens = 
+    let empty = []
+    toTokenList empty
+
 
 [<EntryPoint>]
 let main argv = 
-    testStm1 |> ignore
+    let lb = LexBuffer<char>.FromString "( 1 + 2 )"
+    let tokens = toTokens lb
+
+    printfn "Press any key to continue..."
+    System.Console.ReadLine() |> ignore
     0 // return an integer exit code
